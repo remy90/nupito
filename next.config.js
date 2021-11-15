ddfb2aa879f039ce999fc07d3ef612436f5eb884
Shaun <shaun.gaisie@doctorlink.com>
2021-11-15 22:56:20 +0000

    Imported MUI, updated main page and config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+  rules: [{
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+  {
+    test: /\.jsx?$/,
+    use: ['babel-loader', 'astroturf/loader'],
+  }],
+  trailingSlash: true,
   reactStrictMode: true,
-}
+};
