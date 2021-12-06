39b76ecc873999be6eeeb7fcc4b413e870be6e67
Shaun <shaun.gaisie@doctorlink.com>
2021-12-06 01:01:43 +0000

    db connection test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/Link';
+// @ts-ignore
+import clientPromise from '../lib/mongodb';
 
-export default function Index() {
+interface IProps {
+  isConnected: boolean;
+}
+export default function Index({ isConnected }: IProps) {
   return (
     <Container maxWidth="sm">
       <Head>
@@ -21,7 +26,37 @@ export default function Index() {
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
+
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
       </Box>
     </Container>
   );
+}
+
+
+export async function getServerSideProps(context: any) {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    // @ts-ignore
+    await clientPromise;
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
