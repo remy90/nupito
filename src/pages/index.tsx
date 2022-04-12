b64a985a6f14e2d1fd4b6ce27b937d0d6a424067
Remy <shaun.gaisie@healthhero.com>
2022-04-12 22:21:14 +0100

    vercel homepage  image upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,18 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Image from 'next/image';
-import homePageImg from '../../public/private-assets/home-page.jpeg';
 import type { GetStaticProps, GetStaticPropsContext, NextPage } from 'next';
 import { fileDownload } from './api/gridFSFileDownload';
 import { fileUpload } from './api/gridFSFileUpload';
 
-const HomePage: NextPage = () => (
+const HomePage: NextPage = ({ homePageImagePath }: any) => (
   <Container maxWidth="sm">
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         {'Welcome to Shaun and Charlotte\'s wedapp'}
       </Typography>
       <Box style={{ width: '100%', maxWidth: '30rem' }}>
-        <Image sizes='100%' src={homePageImg} alt="Home page image of shaun and charlotte" />
+        <Image sizes='100%' src={homePageImagePath} alt="Home page image of shaun and charlotte" />
       </Box>
     </Box>
   </Container>
@@ -26,7 +25,7 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
   async function exists(path: string) {  
     return fs.existsSync(path);
   }
-  await fileUpload('home-page.jpeg', 'home-image01', 'public/private-assets/home-page.jpeg');
+  // await fileUpload('home-page.jpeg', 'home-image01', 'public/private-assets/home-page.jpeg');
   
   if (!(await exists('public/home-page.jpeg'))) {
     await fileDownload('home-page.jpeg', 'home-image01', 'public/home-page.jpeg');
