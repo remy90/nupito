89dfe03b8f6072f4194bb49fd5282c98aae0e53f
Remy <shaun.gaisie@healthhero.com>
2022-04-12 21:26:13 +0100

    add homepage image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import Head from 'next/head';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Image from 'next/image';
 import homePageImg from '../../public/private-assets/home-page.jpeg';
 import type { NextPage } from 'next';
+import { fileDownload } from './api/gridFSFileDownload';
+import { fileUpload } from './api/gridFSFileUpload';
 
 const HomePage: NextPage = () => (
   <Container maxWidth="sm">
@@ -19,20 +20,20 @@ const HomePage: NextPage = () => (
     </Box>
   </Container>
 );
-// export async function getStaticProps(context) {
-//   const fs = require('fs');
-//   async function exists(path: string) {  
-//     return fs.existsSync('public/home-page.jpeg');
-//   }
-//   // await fileUpload('home-page.jpeg', 'home-image01', 'public/private-assets/home-page.jpeg');
+export async function getStaticProps(context: any) {
+  const fs = require('fs');
+  async function exists(path: string) {  
+    return fs.existsSync('public/home-page.jpeg');
+  }
+  await fileUpload('home-page.jpeg', 'home-image01', 'public/private-assets/home-page.jpeg');
   
-//   if (!(await exists('public/home-page.jpeg'))) {
-//     await fileDownload('home-page.jpeg', 'home-image01', 'public/home-page.jpeg');
-//   }
+  if (!(await exists('public/home-page.jpeg'))) {
+    await fileDownload('home-page.jpeg', 'home-image01', 'public/home-page.jpeg');
+  }
 
-//   return { props: {
-//     homePageImagePath: '/public/home-page.jpeg'
-//   }};
-// }
+  return { props: {
+    homePageImagePath: '/public/home-page.jpeg'
+  }};
+}
 
 export default HomePage;
