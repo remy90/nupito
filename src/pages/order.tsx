c3e619b6a88e1b6dfbbd1ec2ea6ba86fa134fa2d
Remy <shaun.gaisie@healthhero.com>
2022-08-19 23:52:44 +0100

    Songs of worship

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -11,17 +11,28 @@ const Order = () => {
   return (
     <Container maxWidth="sm">
       <Typography variant="h2" sx={{marginTop}}>Order of service</Typography>
-      <Paper><Timeline>
-        <TimelineElement name="Entrance of the Bride" />
-        <TimelineElement name="Welcome &amp; prayer" />
-        <TimelineElement name="Declatory &amp; contracting words" />
-        <TimelineElement name="Scripture recital" />
-        <TimelineElement name={<Link href="/songs">Worship</Link>} />
-        <TimelineElement name="Sermon" />
-        <TimelineElement name={<Link href="/songs">Worship</Link>} />
-        <TimelineElement name="Vows" />
-        <TimelineElement name="Ring exchange" isLast/>
-      </Timeline>
+      <Paper>
+        <Timeline>
+          <TimelineElement name="Entrance of the Bride" />
+          <TimelineElement name={<Link href="/songs">Hymn: Amazing grace</Link>} />
+          <TimelineElement name="First reading: Phillippians 2:1-5 - Read by Niya Bekele" />
+          <TimelineElement name="Declaration and vows" />
+          <TimelineElement name="Sermon: Reverend Nigel J. Little" />
+          <TimelineElement name="Vows &amp; exchange of the rings &amp; declaration" />
+          <TimelineElement name={<Link href="/songs">Hymn: Our father - Bethel</Link>} />
+          <TimelineElement name={<Link href="/songs">Hymn: Reckless Love - Cory Asbury</Link>} />
+          <TimelineElement name={<Link href="/songs">Hymn: Worthy - John Wilds</Link>} />
+          <TimelineElement name={<Link href="/songs">Hymn: Holy is the Lord God Almighty - Chris Tomlin</Link>} />
+          <TimelineElement name={<Link href="/songs">Hymn: Raise a hallelujah - Jonathan &amp; Melissa Helser</Link>} />
+          <TimelineElement name={<Link href="/songs">Hymn: Jesus name above all names - KXC</Link>} />
+          <TimelineElement name="Second reading: 1 John 4: 7-21" />
+          <TimelineElement name="Poem: Two lives become one - read by Shola Ibitoye" />
+          <TimelineElement name="Sermon 2 - Reverend Nigel J,. Little" />
+          <TimelineElement name="Prayers" />
+          <TimelineElement name="Signing of the register" />
+          <TimelineElement name="The Lords prayer" />
+          <TimelineElement name={<Link href="/songs">Hymn: Ancient of Days - Ron Kenoly</Link>} isLast />
+        </Timeline>
       
       </Paper>
     </Container>
--- src/pages/songs.tsx
+++ src/pages/songs.tsx
@@ -1,13 +1,204 @@
 import type React from 'react';
 import Container from '@mui/material/Container';
 import type { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 import {marginTop} from '../styles/sxConstants';
 
 const Hymns: NextPage = () => (
   <Container maxWidth="sm">
-    <Typography variant="h2" sx={{marginTop}}>Songs of praise</Typography>
-    <Typography>coming soon...</Typography>
+    <Paper sx={{p: '1rem'}}>
+      <Typography variant="h2" sx={{marginTop}}>Songs of praise</Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Amazing grace</strong></Typography>
+      <Typography>
+        Amazing grace! How sweet the sound
+        <br />That saved a wretch like me.
+        <br />I once was lost, but now am found,
+        <br />Was blind, but now I see.
+        <br />
+        <br />Twas grace that taught my heart to fear,
+        <br />And grace my fears relieved.
+        <br />How precious did that grace appear
+        <br />The hour I first believed.
+        <br />
+        <br />Through many dangers, toils and snares
+        <br />I have already come;
+        <br />Tis grace has brought me safe thus far
+        <br />And grace will lead me home.
+        <br />
+        <br />The Lord has promised good to me
+        <br />His word my hope secures;
+        <br />He will my shield and portion be,
+        <br />As long as life endures.
+        <br />
+        <br />Yea, when this flesh and heart shall fail,
+        <br />and mortal life shall cease,
+        <br />I shall possess within the veil,
+        <br />A life of joy and peace.
+        <br />
+        <br />When we&apos;ve been there ten thousand years
+        <br />Bright shining as the sun,
+        <br />We&apos;ve no less days to sing God&apos;s praise
+        <br />Than when we&apos;ve first begun.
+      </Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Our Father - Bethel</strong></Typography>
+      <Typography>
+        <br />Our Father in Heaven
+        <br /> Hallowed be Your name
+        <br /> Your Kingdom come quickly
+        <br /> Your will be done the same
+        <br />On Earth as it is in Heaven
+        <br /> Let Heaven come to earth
+        <br /> As it is in Heaven
+        <br /> Let Heaven come
+        <br /> Our Father in Heaven
+        <br /> Hallowed be Your name
+        <br /> Your Kingdom come quickly
+        <br /> Your will be done the same
+        <br /> On earth as it is in Heaven
+        <br /> Let Heaven come to earth
+        <br /> As it is in Heaven
+        <br />
+        <br /> Let Heaven come
+        <br />
+        <br /> Yours is the Kingdom, Yours is the power
+        <br /> Yours is the glory forever, amen
+      </Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Reckless Love - cory asbury</strong></Typography>
+      <Typography>
+        Before I spoke a word, You were singing over me
+        <br /> You have been so, so good to me
+        <br /> Before I took a breath, You breathed Your life in me
+        <br /> You have been so so kind to me
+        <br />
+        <br />Oh, the overwhelming, never-ending, reckless love of God
+        <br /> Oh, it chases me down, fights &apos;til I&apos;m found, leaves the 99
+        <br /> And I couldn&apos;t earn it
+        <br /> I don&apos;t deserve it, still You give yourself away
+        <br /> Oh, the overwhelming, never-ending, reckless love of God
+        <br />
+        <br />When I was Your foe, still Your love fought for me
+        <br /> You have been so, so good to me
+        <br /> When I felt no worth, You paid it all for me
+        <br /> You have been so, so kind to me
+        <br />
+        <br />There&apos;s no shadow You won&apos;t light up
+        <br /> Mountain You won&apos;t climb up
+        <br /> Coming after me
+        <br /> There&apos;s no wall You won&apos;t kick down
+        <br /> Lie You won&apos;t tear down
+        <br /> Coming after me
+        <br />
+        <br /></Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Worthy - John Wilds</strong></Typography>
+      <Typography>
+        Worthy is Your name, Jesus
+        <br />You deserve the praise
+        <br />Worthy is Your name
+        <br />Worthy is Your name, Jesus
+        <br />You deserve the praise
+        <br />Worthy is Your name
+
+      </Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Holy is the Lord God Almighty - Chris Tomlin</strong></Typography>
+      <Typography>
+        We stand and lift up our hands
+        <br />For the joy of the Lord is our strength
+        <br />We bow down and worship Him now
+        <br />How great, how awesome is He
+        <br />And together we sing
+        <br />
+        <br />Holy is the Lord God almighty
+        <br />The earth is filled with His glory
+        <br />Holy is the Lord God almighty
+        <br />The earth is filled with His glory
+        <br />The earth is filled with His glory
+        <br />
+        <br />It&apos;s rising up all around
+        <br />It&apos;s the anthem of the Lord&apos;s renown [x2]
+        <br />And together we sing
+        <br />Everyone sing
+      </Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Raise a hallelujah - Jonathan &amp; Melissa Helser</strong></Typography>
+      <Typography>
+I raise a hallelujah, in the presence of my enemies
+        <br /> I raise a hallelujah, louder than the unbelief
+        <br /> I raise a hallelujah, my weapon is a melody
+        <br /> I raise a hallelujah, heaven comes to fight for me
+        <br />
+        <br />I&apos;m gonna sing in the middle of the storm
+        <br /> Louder and louder, you&apos;re gonna hear my praises roar
+        <br /> Up from the ashes hope will arise
+        <br /> Death is defeated, the King is alive
+        <br />
+        <br />I raise a hallelujah, with everything inside of me
+        <br /> I raise a hallelujah, I will watch the darkness flee
+        <br /> I raise a hallelujah, in the middle of the mystery
+        <br /> I raise a hallelujah, fear, you lost your hold on me
+        <br />
+        <br />Sing a little louder - In the presence of my enemies
+        <br />Sing a little louder - Louder than the unbelief
+        <br />Sing a little louder - My weapon is a melody
+        <br />Sing a little louder - Heaven comes to fight for me
+
+        <br />I raise a hallelujah (hallelujah)
+        <br />I raise a hallelujah (I raise my hallelujah)
+      </Typography>
+      <Typography sx={{mt: '1rem'}}><strong>Jesus name above all names - KXC</strong></Typography>
+      <Typography>
+        To the mountains that I face
+        <br />There&apos;s one name that&apos;s higher.
+        <br />To the empires of our age
+        <br />There&apos;s one name that&apos;s greater
+        <br />
+        <br />To the giants in my way
+        <br />There&apos;s one name that&apos;s higher.
+        <br />To the evil that prevails
+        <br />There&apos;s one name that&apos;s greater
+        <br />
+        <br />Jesus, You&apos;re the name above all names
+        <br />Over my fears over the storms
+        <br />I&apos;m gonna sing that Jesus is Lord
+        <br />Jesus, You&apos;re the name above all names
+        <br />I&apos;m gonna sing, that Jesus is Lord
+        <br />
+        <br />To the burden of my shame
+        <br />There&apos;s one name that&apos;s higher.
+        <br />To the battles that still rage
+        <br />There&apos;s one name that&apos;s greater
+        <br />
+        <br />To the doubts that steal my faith
+        <br />There&apos;s one name that&apos;s higher
+        <br />To the idols that enslave 
+        <br />There&apos;s one name that&apos;s greater
+        <br />
+        <br />The miracle making,
+        <br />City wall shaking
+        <br />Prodigal chasing 
+        <br />Lazarus raising
+        <br />Destiny waking
+        <br />History changing 
+        <br />Powerful name of God
+      </Typography>
+
+      <Typography sx={{mt: '1rem'}}><strong>Ancient of days - Ron Kenoly</strong></Typography>
+      <Typography>
+Blessing and honour, glory and power
+        <br />Be unto the Ancient of Days
+        <br />From every nation, all of creation
+        <br />Bow before the Ancient of Days
+        <br />
+        <br />Every tongue in heaven and earth shall declare Your glory
+        <br />Every knee shall bow at Your throne in worship
+        <br />You will be exalted, oh God
+        <br />And Your kingdom shall not pass away
+        <br />Oh, Ancient of Days
+        <br />
+        <br />Your kingdom shall reign over all the earth
+        <br />Sing unto the Ancient of Days
+        <br />For none can compare to Your matchless worth
+        <br />Sing unto the Ancient of Days
+      </Typography>
+    </Paper>
   </Container>
 );
 
